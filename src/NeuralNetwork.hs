b02f523d12f39ffcc76ccb7c6034dd8c4dd3583a
McKay Broderick <mckay.db@gmail.com>
2019-05-26 16:02:58 -0600

    Added the ability to choose the initial weights

--- src/NeuralNetwork.hs
+++ src/NeuralNetwork.hs
@@ -7,6 +7,8 @@ import qualified Data.Vector as V
 import qualified Data.Matrix as M
 import qualified System.Random as R
 
+-- TODO: Cleanup the types here. They're... not useful
+
 sigmoid :: (Num a, Fractional a, Floating a) => a -> a
 sigmoid x = 1 / (1 + exp (-x))
 
@@ -16,15 +18,18 @@ sigmoid' x = x * (1 - x)
 train :: Integer -> Matrix Double -> Matrix Double -> IO (Matrix Double)
 train iterations inputs outputs = do
   initialWeights' <- V.generateM (M.nrows inputs - 1) (const (R.randomRIO (-1, 1) :: IO Double))
-  pure $ foldr go (M.colVector initialWeights') [1..iterations]
+  pure $ foldr (loop inputs outputs) (M.colVector initialWeights') [1..iterations]
+
+train' :: Integer -> Matrix Double -> Matrix Double -> Matrix Double -> Matrix Double
+train' iterations initialWeights inputs outputs = foldr (loop inputs outputs) initialWeights [1..iterations]
+
+loop :: Matrix Double -> Matrix Double -> Integer -> Matrix Double -> Matrix Double
+loop inputs outputs _ weights = weights + adjustments
   where
-    go :: Integer -> Matrix Double -> Matrix Double
-    go _ weights =
-      let newOutput = think weights inputs
-          error = outputs - newOutput
-          changes = sigmoid' <$> newOutput
-          adjustments = M.multStd (M.transpose inputs) (M.multStd error (M.transpose changes))
-       in weights + adjustments
+    newOutput = think weights inputs
+    error = outputs - newOutput
+    changes = sigmoid' <$> newOutput
+    adjustments = M.multStd (M.transpose inputs) (M.multStd error (M.transpose changes))
 
 think :: Matrix Double -> Matrix Double -> Matrix Double
 think weights inputs = sigmoid <$> M.multStd inputs weights
